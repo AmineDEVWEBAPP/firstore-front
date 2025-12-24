8fa019e513f899299af371b9494471695decdb12
root <root@localhost>
2025-12-24 10:19:32 +0100

    update admin auth structer

--- src/core/middleware/adminAuth.jsx
+++ src/core/middleware/adminAuth.jsx
@@ -1,7 +1,7 @@
 import { redirect } from "react-router-dom"
 import AdminServices from "../services/admin_services"
 
-export default async function adminLogged() {
+export default async function adminAuth() {
     const logged = await AdminServices.logged()
-    if (!logged) throw redirect('/dashboard/login')
+    if (logged) throw redirect('/dashboard/home')
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,29 +3,27 @@ import { createRoot } from 'react-dom/client'
 import './view/index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import DashboardLogin from './view/dashboard/login/dashboardLogin.jsx'
-import AdminLogged from './core/middleware/adminLogged.jsx'
 import Dashboard from './view/dashboard/dashboard/dashboard.jsx'
 import DashboardHome from './view/dashboard/home/home.jsx'
 import Offers from './view/dashboard/offers/offers.jsx'
 import Accounts from './view/dashboard/accounts/accounts.jsx'
 import Profiles from './view/dashboard/profiles/profiles.jsx'
 import Users from './view/dashboard/users/users.jsx'
+import DashboardHomeController from './controller/dashboardHome.js'
+import adminAuth from './core/middleware/adminAuth.jsx'
+import adminLogged from './core/middleware/adminLogged.jsx'
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path='dashboard'>
-        <Route element={<AdminLogged page='login' />}>
-          <Route path='login' element={<DashboardLogin />} />
-        </Route>
-        <Route element={<AdminLogged />}>
-          <Route element={<Dashboard />}>
-            <Route index path='home' element={<DashboardHome />} />
-            <Route path='offers' element={<Offers />} />
-            <Route path='accounts' element={<Accounts />} />
-            <Route path='profiles' element={<Profiles />} />
-            <Route path='users' element={<Users />} />
-          </Route>
+        <Route path='login' element={<DashboardLogin />} loader={adminAuth} />
+        <Route element={<Dashboard />} loader={adminLogged}>
+          <Route index path='home' element={<DashboardHome />} loader={DashboardHomeController.initData} />
+          <Route path='offers' element={<Offers />} />
+          <Route path='accounts' element={<Accounts />} />
+          <Route path='profiles' element={<Profiles />} />
+          <Route path='users' element={<Users />} />
         </Route>
       </Route>
     </Route>
